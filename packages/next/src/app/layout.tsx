006caf1f7fbf972c8cafceeb02db54525cca5e09
Loic Masson <29038716+loicmasson2@users.noreply.github.com>
2024-08-03 08:37:09 +0300

    Clean clerk and uploadthing to make the build pass

--- packages/next/src/app/layout.tsx
+++ packages/next/src/app/layout.tsx
@@ -1,14 +1,8 @@
 import "~/styles/globals.css";
-import "@uploadthing/react/styles.css";
-
-// import { ClerkProvider } from "@clerk/nextjs";
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 import { TopNav } from "./_components/topnav";
-import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
-import { ourFileRouter } from "~/app/api/uploadthing/core";
-import { extractRouterConfig } from "uploadthing/server";
 import { Toaster } from "~/components/ui/sonner";
 
 export const metadata: Metadata = {
@@ -22,17 +16,7 @@ export default function RootLayout({
   modal,
 }: Readonly<{ children: React.ReactNode; modal: React.ReactNode }>) {
   return (
-    // <ClerkProvider>
       <html lang="en" className={`${GeistSans.variable}`}>
-        <NextSSRPlugin
-          /**
-           * The `extractRouterConfig` will extract **only** the route configs
-           * from the router to prevent additional information from being
-           * leaked to the client. The data passed to the client is the same
-           * as if you were to fetch `/api/uploadthing` directly.
-           */
-          routerConfig={extractRouterConfig(ourFileRouter)}
-        />
         <body className={`font-sans ${GeistSans.variable} dark`}>
           <div className={"grid h-screen grid-rows-[auto,1fr]"}>
             <TopNav />
@@ -43,6 +27,5 @@ export default function RootLayout({
           <Toaster />
         </body>
       </html>
-    // </ClerkProvider>
   );
 }
