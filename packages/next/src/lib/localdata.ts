32efaf6e8881769ccc4875177052b7ed551ec4b8
Loic Masson <29038716+loicmasson2@users.noreply.github.com>
2024-08-12 20:39:13 +0300

    Change path

--- packages/next/src/lib/localdata.ts
+++ packages/next/src/lib/localdata.ts
@@ -6,7 +6,7 @@ import {processEnv} from "@next/env";
 export async function getLocalData() {
   // Get the path of the json file
   // const filePath = "./src/app/json/data.json";
-  const prefix = processEnv().NODE_ENV === "development" ? "./src/app/" : "/";
+  const prefix = processEnv().NODE_ENV === "development" ? "./src/app/" : "./";
   const filePath = path.join(prefix, "json/data.json");
   // Read the json file
   const jsonData = await fsPromises.readFile(filePath);
@@ -19,7 +19,7 @@ export async function getLocalData() {
 
 export async function getLocalDataForOneHouse(id: string) {
   // Get the path of the json file
-  const prefix = processEnv().NODE_ENV === "development" ? "./src/app/" : "/";
+  const prefix = processEnv().NODE_ENV === "development" ? "./src/app/" : "./";
   const filePath = path.join(prefix, "json/data.json");
 
 
