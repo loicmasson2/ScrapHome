2915261546fe91346c3f87029c78bf68966de9ec
Loic Masson <29038716+loicmasson2@users.noreply.github.com>
2024-08-12 20:31:24 +0300

    Prefix per node env

--- packages/next/src/lib/localdata.ts
+++ packages/next/src/lib/localdata.ts
@@ -1,14 +1,13 @@
 import fsPromises from "fs/promises";
 import path from "path";
 import { House } from "~/lib/definitions";
-// import * as process from "node:process";
+import {processEnv} from "@next/env";
 
 export async function getLocalData() {
-  console.info("HELLO", process.cwd())
   // Get the path of the json file
   // const filePath = "./src/app/json/data.json";
-
-  const filePath = "~/app/json/data.json";
+  const prefix = processEnv().NODE_ENV === "development" ? "./src/app/" : "/";
+  const filePath = path.join(prefix, "json/data.json");
   // Read the json file
   const jsonData = await fsPromises.readFile(filePath);
   // Parse data as json
@@ -19,9 +18,10 @@ export async function getLocalData() {
 }
 
 export async function getLocalDataForOneHouse(id: string) {
-  console.info("HELLO", process.cwd())
   // Get the path of the json file
-  const filePath = "~/app/json/data.json";
+  const prefix = processEnv().NODE_ENV === "development" ? "./src/app/" : "/";
+  const filePath = path.join(prefix, "json/data.json");
+
 
   // Read the json file
   const jsonData = await fsPromises.readFile(filePath);
